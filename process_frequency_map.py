3d886dbcc4be8c6fe0a227fd4fb961f90647f87b
Sam Behrens <sbehrens1414@gmail.com>
2022-02-21 13:46:55 -0800

    Add processing of words

---
+++ process_frequency_map.py
@@ -0,0 +1,44 @@
+import json
+from pprint import pprint, pformat
+
+def main():
+    with open("frequency_map.json") as f:
+        data = json.load(f)
+
+    in_order = sorted(data.items(), key=lambda item: item[1])
+    in_order = [tup[0] for tup in in_order]
+
+    with open("output.txt") as f:
+        try:
+            last_word = f.readlines()[-1].split(",")[0]
+            print(f'last word rated: {last_word}')
+
+            if last_word:
+                in_order = in_order[in_order.index(last_word) + 1:]
+
+            print(in_order[:5])
+        except:
+            print("No entries yet")
+
+
+    with open("output.txt", "a") as f:
+        last_input = ""
+        for word in in_order:
+            accepted = False
+            while not accepted:
+                rating = input(f'{word} (1: good, 2: eh, 3: bad): ')
+                try:
+                    if rating.strip() == "" and last_input:
+                        rating = last_input
+                    else:
+                        rating = int(rating)
+                    last_input = rating
+                    f.writelines(f'{word},{rating}\n')
+                    accepted = True
+                except:
+                    print("Input not accepted, retry")
+        #  f.write(pformat(in_order))
+
+
+if __name__ == "__main__":
+    main()
